e8150f439bd0abe9cb0b40075e8385377e0943e4
aogheliandlv <aoghelian@doughertylaservision.com>
2021-05-22 14:34:50 -0700

    repair

--- generateexcel.py
+++ generateexcel.py
@@ -1,9 +1,9 @@
 
 import pandas as pd
 
-print ('4 letter word password generator')
+#print ('4 letter word password generator')
 
 filename = "list.xlsx"
 
-df = pd.read_excel(filename, sheet_name = "sheet1", engine='openpyxl')
+df = pd.read_excel(filename, sheet_name = "Sheet1", engine='openpyxl')
 df.head()
